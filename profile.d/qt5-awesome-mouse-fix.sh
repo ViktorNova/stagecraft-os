eda36be932693e63d16325d1cb95e2c0d73ca41f
Viktor Nova <viktor@pinknoiseboys.com>
2016-04-13 23:37:56 -0700

    Added files for /etc/profile.d

---
+++ profile.d/qt5-awesome-mouse-fix.sh
@@ -0,0 +1,3 @@
+#Fixes Awesome panels not working when QT apps are running
+export QT_XCB_NO_XI2_MOUSE=1
+
---
+++ profile.d/qt5-theme.sh
@@ -0,0 +1,2 @@
+#Forces QT to use the theme set by Qt5ct
+export QT_QPA_PLATFORMTHEME=qt5ct
